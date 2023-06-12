6510bc93cbd81712a86eaf7e28633e245bcc44b9
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-13 01:43:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello guys! chai pee lo</h1>
+      <h2>ooo garam hai!</h2>
+    </div>
+  );
+}
+
+export default App;
