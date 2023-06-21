e23480a6097966a868488ad93e54bc4dbb29f23a
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-21 06:38:02 +0530

    fix: add posts data

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
 
 /**
@@ -8,29 +7,329 @@ import { formatDate } from "../utils/authUtils";
 
 export const posts = [
   {
-    _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    _id: "688e6e59-4d2a-4b7e-a8b9-7b3ad1de1b84",
+    content: `In India, everyone is a self taught developer.
+      
+      Fact. Not kidding. The education part of college failed all of us.`,
+    mediaURL: "",
     likes: {
-      likeCount: 0,
+      likeCount: 8955,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "adarshbalika",
+    username: "tanaypratap",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    comments: []
   },
   {
-    _id: uuid(),
+    _id: "126faa8e-239c-4ef4-9320-93d416685746",
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "🌟 Big announcement! 🎉I've just finished building my dazzling jewellery website using React! 💍💎It's been an amazing adventure, merging my love for web development and exquisite accessories. Explore the collection, share your thoughts. #React #webdevelopment #programming #FrontEnd",
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687272784/Social%20Media/QBxlHtDX6hSLXsy9_rclper.mp4",
     likes: {
-      likeCount: 0,
+      likeCount: 53,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "shubhamsoni",
+    username: "TheIndianGirl56",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "f1a16ff7-815c-4529-aa72-97d17f5437c2",
+    content: `Dear Government or whoever is authorized, 
+
+      PLEASE Work on your sites. They are slow and at times don't function at all. 
+      
+      Hire me. I will make it smooth. 
+      That too for free. 
+      
+      - A frustrated user.
+      Isha.`,
+    mediaURL: "",
+    likes: {
+      likeCount: 2202,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shaaaviii",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "6e628b4f-3f84-4322-8914-0cc37f27a6a3",
+    content: `Tried my hands on another tutorial. Credits to @VarunMayya and @UnrealSensei. You are make my learning easy everyday.
+    
+    #madewithUnreal`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687273193/Social%20Media/N5qCMBoBRXj7Vhg1_q1xnax.mp4",
+    likes: {
+      likeCount: 16,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "BeingPoem",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "c56eb64c-578b-419f-9f6a-ff8ffed47d2f",
+    content: `Finally the wait is over. Very excited to start the first lecture tomorrow.
+    @neogcamp @tanaypratap @ch_akanksha @tanviprya
+    
+    Grateful ✨`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687273458/Social%20Media/FooAmf1aMAY2pCE_manxd3.jpg",
+    likes: {
+      likeCount: 56,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "ManishaG15",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "db6b0d30-7a9f-4f4d-89a3-56c827536112",
+    content: `Namaste!🤩🙏
+
+    I just completed READHAVEN, my first React e-commerce web app dedicated to books!🚀📚
+    
+    Kindly spread the word & share your love for books!📢
+    Feedback & suggestions are highly appreciated! 💙
+    
+    Feel free to explore & let me know what you think! Happy book shopping!🛒`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274033/Social%20Media/Screen_Recording_dniohf.mp4",
+    likes: {
+      likeCount: 80,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "Niharika_twt",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "3f5f4e7a-df8c-4a68-8cb6-5db4c0a7d76e",
+    content: `Received an awesome surprise on Women's Day from @hashnode
+    Got to be a part of 20 noteworthy women writers from Hashnode. 😭
+    
+    Celebrating this small win only because of @tanaypratap Bhaiya and my lovely @neogcamp family! ❤️🙌
+    #alwaysgrateful #WomensDay`,
+    mediaURL: "",
+    likes: {
+      likeCount: 95,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "pranita0709",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "a9a1c543-88d0-4d0d-9c68-28dc51e0c2c5",
+    content: `🚀Finally completed my first #React ecommerce app! 💻 It's been a wild ride of learning and coding, but I'm super happy of the outcome. Grateful to @neogcamp 💙
+    Feedback & suggestions are highly appreciated! 💜
+    Live: https://eyesome.netlify.app   
+    Github repo: https://github.com/SandhyaR1007/eyesome-react`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274491/Social%20Media/HyDednSN9Ev0xiY5_ezewyz.mp4",
+    likes: {
+      likeCount: 801,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "SandhyaR1007",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "7841a9b2-9c8d-46d1-9240-08b5d40223ce",
+    content: `Thank you @Spotify to celebrate our 10th anniversary together!💜 #BTSARMY, check out 'My Top 5 BTS Songs' page at http://spotify.com/top5!
+    
+    #BTS #방탄소년단 #BTS10thAnniversary`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274962/Social%20Media/LH5Bt06ejticAH9n_qsi6ub.mp4",
+    likes: {
+      likeCount: 239800,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "bts_bighit",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "77a5c0a7-9083-46f2-8b59-f78870811d45",
+    content: `Last day at Microsoft. Emotional.
+
+    Into fixing education 200% now. Ask Me Anything.`,
+    mediaURL: "",
+    likes: {
+      likeCount: 3911,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "tanaypratap",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "d2d3fb09-781e-44ed-a55a-33f2391d0e47",
+    content: `Attended an incredible session by @tanaypratap on testing web applications with Jest! 🚀💻Learned so much and can't wait to apply those to my projects. #TDD Thank you for the insightful session! #RGR #Testing #Jest #WebDevelopment #reactjs #javascript #library #thankyou #projects`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687275388/Social%20Media/FyWpe8YWcAIe8ms_weocim.jpg",
+    likes: {
+      likeCount: 17,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "TheIndianGirl56",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "a36638f4-b48b-4bbd-aaf2-93828e827c46",
+    content: `Yes Bangalore is beautiful ❣
+    But have you been in Kerela?`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687275635/Social%20Media/FnkBxRuaMAIemSh_bchrpl.jpg",
+    likes: {
+      likeCount: 31,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shaaaviii",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "90545a39-87c7-4b3c-bd35-ebdcd0788a14",
+    content: `Here's a small outcome of learnings from last 2 weeks.
+    Level, Lighting and Cinematics made in unreal. Characters and Animations are from Mixamo. 
+    Mentor : @VarunMayya
+    
+    Full video link :https://youtu.be/WbqcLrv73fg
+    
+    #unrealengine #animations #sequencer #mixamo #gamedev`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687275816/Social%20Media/sWyXfmp6VU8-_3UX_tcznte.mp4",
+    likes: {
+      likeCount: 9,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "BeingPoem",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "6c420b4e-9dfe-4d35-80e9-597a0459e918",
+    content: `Finally..!! INDIA WON THE ASIA CUP 2016☆☆☆`,
+    mediaURL: "",
+    likes: {
+      likeCount: 11,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "ManishaG15",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "fbd4a746-41c7-4c26-9980-1fb4ef5e69af",
+    content: `I'm now a 4 kyū blue belt on @codewars! Come join me #codewarriors!.`,
+    mediaURL: "",
+    likes: {
+      likeCount: 23,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "Niharika_twt",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "32dbf8f8-6890-4c89-9fd9-3de6a0cb05d7",
+    content: `Exciting news! 
+    Finally completed my e-commerce app for plant lovers using React! 
+    It's been an incredible journey, learning web development and combining my passions. 
+    Check it out and share your feedback! 
+    #reactjs #css #ecommerce #plants #webdevelopment #WomenInTech #neogcamp`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687307844/Social%20Media/F86vuXe5jvBQeUwE_dla6ky.mp4",
+    likes: {
+      likeCount: 76,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "pranita0709",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "84c9e418-2ed3-471e-8b7a-c73e9f3d8b78",
+    content: `Had an amazing session on Call Stack with @tanaypratap sir. I've never had a chance to learn from someone who makes you understand things so effortlessly. I'm grateful to be a part of @neogcamp 2023. I feel like haule haule I'll also get there. 💫🙏🏻💜
+    #haulehaule #iykyk`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687307973/Social%20Media/FqZmyW_WcAAEjnC_xb9nbj.jpg",
+    likes: {
+      likeCount: 66,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "SandhyaR1007",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "9c12aee1-6b7e-4ad6-99f2-2028f192692f",
+    content: `#StopAsianHate #StopAAPIHate`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687308608/Social%20Media/ExsRBTSVIAIm2jr_nhsjcz.jpg",
+    likes: {
+      likeCount: 2400000,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "bts_bighit",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "b67d07e7-4e72-46a0-946a-7b3bcbf39e59",
+    content: `The pink flowers in BLR are breathtaking… Why are you missing this?`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687308834/Social%20Media/Fr5eSAZaUAANSXm_foqnlf.jpg",
+    likes: {
+      likeCount: 450,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "tanaypratap",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
+  },
+  {
+    _id: "54fd5411-50c1-4a0a-b650-2e580b503d46",
+    content: `Waiting for the day we will meet again after the end of warm winter, the record of Tuesday, December 13th 🎞️ Our Jin!! Come back safely!! 💜Love You💜 (https://youtu.be/L-orDkbsuHk)`,
+    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687309035/Social%20Media/Fj_07TdXwAIb8W5_z5fkku.jpg",
+    likes: {
+      likeCount: 2300000,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "bts_bighit",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comments: []
   },
 ];
