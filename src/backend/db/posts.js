99de4bcd2f63c011a5be7ae57b551094578b29cf
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-26 04:47:35 +0530

    data: updated date in db

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -18,24 +18,25 @@ export const posts = [
       dislikedBy: [],
     },
     username: "tanaypratap",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2021-07-25T19:57:00+05:30",
+    updatedAt: "2021-07-25T19:57:00+05:30",
+    comments: [],
   },
   {
     _id: "126faa8e-239c-4ef4-9320-93d416685746",
     content:
       "🌟 Big announcement! 🎉I've just finished building my dazzling jewellery website using React! 💍💎It's been an amazing adventure, merging my love for web development and exquisite accessories. Explore the collection, share your thoughts. #React #webdevelopment #programming #FrontEnd",
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687272784/Social%20Media/QBxlHtDX6hSLXsy9_rclper.mp4",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687272784/Social%20Media/QBxlHtDX6hSLXsy9_rclper.mp4",
     likes: {
       likeCount: 53,
       likedBy: [],
       dislikedBy: [],
     },
     username: "TheIndianGirl56",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-06-14T20:11:00+05:30",
+    updatedAt: "2023-06-14T20:11:00+05:30",
+    comments: [],
   },
   {
     _id: "f1a16ff7-815c-4529-aa72-97d17f5437c2",
@@ -55,25 +56,26 @@ export const posts = [
       dislikedBy: [],
     },
     username: "shaaaviii",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2022-09-12T15:52:00+05:30",
+    updatedAt: "2022-09-12T15:52:00+05:30",
+    comments: [],
   },
   {
     _id: "6e628b4f-3f84-4322-8914-0cc37f27a6a3",
     content: `Tried my hands on another tutorial. Credits to @VarunMayya and @UnrealSensei. You are make my learning easy everyday.
     
     #madewithUnreal`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687273193/Social%20Media/N5qCMBoBRXj7Vhg1_q1xnax.mp4",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687273193/Social%20Media/N5qCMBoBRXj7Vhg1_q1xnax.mp4",
     likes: {
       likeCount: 16,
       likedBy: [],
       dislikedBy: [],
     },
     username: "BeingPoem",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2020-12-06T20:41:00+05:30",
+    updatedAt: "2020-12-06T20:41:00+05:30",
+    comments: [],
   },
   {
     _id: "c56eb64c-578b-419f-9f6a-ff8ffed47d2f",
@@ -81,16 +83,17 @@ export const posts = [
     @neogcamp @tanaypratap @ch_akanksha @tanviprya
     
     Grateful ✨`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687273458/Social%20Media/FooAmf1aMAY2pCE_manxd3.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687273458/Social%20Media/FooAmf1aMAY2pCE_manxd3.jpg",
     likes: {
       likeCount: 56,
       likedBy: [],
       dislikedBy: [],
     },
     username: "ManishaG15",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-02-11T12:00:00+05:30",
+    updatedAt: "2023-02-11T12:00:00+05:30",
+    comments: [],
   },
   {
     _id: "db6b0d30-7a9f-4f4d-89a3-56c827536112",
@@ -102,16 +105,17 @@ export const posts = [
     Feedback & suggestions are highly appreciated! 💙
     
     Feel free to explore & let me know what you think! Happy book shopping!🛒`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274033/Social%20Media/Screen_Recording_dniohf.mp4",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274033/Social%20Media/Screen_Recording_dniohf.mp4",
     likes: {
       likeCount: 80,
       likedBy: [],
       dislikedBy: [],
     },
     username: "Niharika_twt",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-06-03T16:53:00+05:30",
+    updatedAt: "2023-06-03T16:53:00+05:30",
+    comments: [],
   },
   {
     _id: "3f5f4e7a-df8c-4a68-8cb6-5db4c0a7d76e",
@@ -127,9 +131,9 @@ export const posts = [
       dislikedBy: [],
     },
     username: "pranita0709",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-03-09T10:53:00+05:30",
+    updatedAt: "2023-03-09T10:53:00+05:30",
+    comments: [],
   },
   {
     _id: "a9a1c543-88d0-4d0d-9c68-28dc51e0c2c5",
@@ -137,32 +141,34 @@ export const posts = [
     Feedback & suggestions are highly appreciated! 💜
     Live: https://eyesome.netlify.app   
     Github repo: https://github.com/SandhyaR1007/eyesome-react`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274491/Social%20Media/HyDednSN9Ev0xiY5_ezewyz.mp4",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274491/Social%20Media/HyDednSN9Ev0xiY5_ezewyz.mp4",
     likes: {
       likeCount: 801,
       likedBy: [],
       dislikedBy: [],
     },
     username: "SandhyaR1007",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-06-05T23:13:00+05:30",
+    updatedAt: "2023-06-05T23:13:00+05:30",
+    comments: [],
   },
   {
     _id: "7841a9b2-9c8d-46d1-9240-08b5d40223ce",
     content: `Thank you @Spotify to celebrate our 10th anniversary together!💜 #BTSARMY, check out 'My Top 5 BTS Songs' page at http://spotify.com/top5!
     
     #BTS #방탄소년단 #BTS10thAnniversary`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274962/Social%20Media/LH5Bt06ejticAH9n_qsi6ub.mp4",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687274962/Social%20Media/LH5Bt06ejticAH9n_qsi6ub.mp4",
     likes: {
       likeCount: 239800,
       likedBy: [],
       dislikedBy: [],
     },
     username: "bts_bighit",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-06-16T18:08:00+05:30",
+    updatedAt: "2023-06-16T18:08:00+05:30",
+    comments: [],
   },
   {
     _id: "77a5c0a7-9083-46f2-8b59-f78870811d45",
@@ -176,38 +182,40 @@ export const posts = [
       dislikedBy: [],
     },
     username: "tanaypratap",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2021-11-12T15:08:00+05:30",
+    updatedAt: "2021-11-12T15:08:00+05:30",
+    comments: [],
   },
   {
     _id: "d2d3fb09-781e-44ed-a55a-33f2391d0e47",
     content: `Attended an incredible session by @tanaypratap on testing web applications with Jest! 🚀💻Learned so much and can't wait to apply those to my projects. #TDD Thank you for the insightful session! #RGR #Testing #Jest #WebDevelopment #reactjs #javascript #library #thankyou #projects`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687275388/Social%20Media/FyWpe8YWcAIe8ms_weocim.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687275388/Social%20Media/FyWpe8YWcAIe8ms_weocim.jpg",
     likes: {
       likeCount: 17,
       likedBy: [],
       dislikedBy: [],
     },
     username: "TheIndianGirl56",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-06-11T21:37:00+05:30",
+    updatedAt: "2023-06-11T21:37:00+05:30",
+    comments: [],
   },
   {
     _id: "a36638f4-b48b-4bbd-aaf2-93828e827c46",
     content: `Yes Bangalore is beautiful ❣
     But have you been in Kerela?`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687275635/Social%20Media/FnkBxRuaMAIemSh_bchrpl.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687275635/Social%20Media/FnkBxRuaMAIemSh_bchrpl.jpg",
     likes: {
       likeCount: 31,
       likedBy: [],
       dislikedBy: [],
     },
     username: "shaaaviii",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-01-28T19:02:00+05:30",
+    updatedAt: "2023-01-28T19:02:00+05:30",
+    comments: [],
   },
   {
     _id: "90545a39-87c7-4b3c-bd35-ebdcd0788a14",
@@ -218,16 +226,17 @@ export const posts = [
     Full video link :https://youtu.be/WbqcLrv73fg
     
     #unrealengine #animations #sequencer #mixamo #gamedev`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687275816/Social%20Media/sWyXfmp6VU8-_3UX_tcznte.mp4",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687275816/Social%20Media/sWyXfmp6VU8-_3UX_tcznte.mp4",
     likes: {
       likeCount: 9,
       likedBy: [],
       dislikedBy: [],
     },
     username: "BeingPoem",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2020-12-17T21:49:00+05:30",
+    updatedAt: "2020-12-17T21:49:00+05:30",
+    comments: [],
   },
   {
     _id: "6c420b4e-9dfe-4d35-80e9-597a0459e918",
@@ -239,9 +248,9 @@ export const posts = [
       dislikedBy: [],
     },
     username: "ManishaG15",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2016-03-06T23:46:00+05:30",
+    updatedAt: "2016-03-06T23:46:00+05:30",
+    comments: [],
   },
   {
     _id: "fbd4a746-41c7-4c26-9980-1fb4ef5e69af",
@@ -253,9 +262,9 @@ export const posts = [
       dislikedBy: [],
     },
     username: "Niharika_twt",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-04-14T03:32:00+05:30",
+    updatedAt: "2023-04-14T03:32:00+05:30",
+    comments: [],
   },
   {
     _id: "32dbf8f8-6890-4c89-9fd9-3de6a0cb05d7",
@@ -264,72 +273,77 @@ export const posts = [
     It's been an incredible journey, learning web development and combining my passions. 
     Check it out and share your feedback! 
     #reactjs #css #ecommerce #plants #webdevelopment #WomenInTech #neogcamp`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687307844/Social%20Media/F86vuXe5jvBQeUwE_dla6ky.mp4",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/video/upload/v1687307844/Social%20Media/F86vuXe5jvBQeUwE_dla6ky.mp4",
     likes: {
       likeCount: 76,
       likedBy: [],
       dislikedBy: [],
     },
     username: "pranita0709",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-06-02T20:13:00+05:30",
+    updatedAt: "2023-06-02T20:13:00+05:30",
+    comments: [],
   },
   {
     _id: "84c9e418-2ed3-471e-8b7a-c73e9f3d8b78",
     content: `Had an amazing session on Call Stack with @tanaypratap sir. I've never had a chance to learn from someone who makes you understand things so effortlessly. I'm grateful to be a part of @neogcamp 2023. I feel like haule haule I'll also get there. 💫🙏🏻💜
     #haulehaule #iykyk`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687307973/Social%20Media/FqZmyW_WcAAEjnC_xb9nbj.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687307973/Social%20Media/FqZmyW_WcAAEjnC_xb9nbj.jpg",
     likes: {
       likeCount: 66,
       likedBy: [],
       dislikedBy: [],
     },
     username: "SandhyaR1007",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-03-05T01:15:00+05:30",
+    updatedAt: "2023-03-05T01:15:00+05:30",
+    comments: [],
   },
   {
     _id: "9c12aee1-6b7e-4ad6-99f2-2028f192692f",
     content: `#StopAsianHate #StopAAPIHate`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687308608/Social%20Media/ExsRBTSVIAIm2jr_nhsjcz.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687308608/Social%20Media/ExsRBTSVIAIm2jr_nhsjcz.jpg",
     likes: {
       likeCount: 2400000,
       likedBy: [],
       dislikedBy: [],
     },
     username: "bts_bighit",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2021-03-30T07:18:00+05:30",
+    updatedAt: "2021-03-30T07:18:00+05:30",
+    comments: [],
   },
   {
     _id: "b67d07e7-4e72-46a0-946a-7b3bcbf39e59",
     content: `The pink flowers in BLR are breathtaking… Why are you missing this?`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687308834/Social%20Media/Fr5eSAZaUAANSXm_foqnlf.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687308834/Social%20Media/Fr5eSAZaUAANSXm_foqnlf.jpg",
     likes: {
       likeCount: 450,
       likedBy: [],
       dislikedBy: [],
     },
     username: "tanaypratap",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2023-03-23T16:01:00+05:30",
+    updatedAt: "2023-03-23T16:01:00+05:30",
+    comments: [],
   },
   {
     _id: "54fd5411-50c1-4a0a-b650-2e580b503d46",
     content: `Waiting for the day we will meet again after the end of warm winter, the record of Tuesday, December 13th 🎞️ Our Jin!! Come back safely!! 💜Love You💜 (https://youtu.be/L-orDkbsuHk)`,
-    mediaURL: "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687309035/Social%20Media/Fj_07TdXwAIb8W5_z5fkku.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dwdsvmvkq/image/upload/v1687309035/Social%20Media/Fj_07TdXwAIb8W5_z5fkku.jpg",
     likes: {
       likeCount: 2300000,
       likedBy: [],
       dislikedBy: [],
     },
     username: "bts_bighit",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-    comments: []
+    createdAt: "2022-12-13T13:08:00+05:30",
+    updatedAt: "2022-12-13T13:08:00+05:30",
+    comments: [],
   },
 ];
