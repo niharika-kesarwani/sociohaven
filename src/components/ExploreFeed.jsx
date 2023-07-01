c02b86875589a3b2acb7b69baa7ab5c594d89b68
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-07-01 05:53:44 +0530

    fix: minor fix

--- src/components/ExploreFeed.jsx
+++ src/components/ExploreFeed.jsx
@@ -4,16 +4,19 @@ import { PostCard } from "./PostCard";
 export const ExploreFeed = () => {
   const {
     post: { allPosts },
-    sortedPosts,
   } = usePost();
 
-  const displayPosts = sortedPosts(allPosts);
+  const displayPosts = allPosts?.sort((a, b) => {
+    const dateA = new Date(a.createdAt);
+    const dateB = new Date(b.createdAt);
+    return dateB - dateA;
+  });
 
   return (
     <div className="flex grow justify-center overflow-y-auto border-r px-3 py-5 md:px-5">
       <div className="flex w-full max-w-lg flex-col items-start gap-5">
         {displayPosts?.length > 0 ? (
-          <ul className="flex flex-col gap-5">
+          <ul className="flex w-full flex-col gap-5">
             {displayPosts?.map((post) => {
               return <PostCard post={post} key={post?._id} />;
             })}
